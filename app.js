92805668a3e23742f4a86f1982199eb5fecf0bc6
Matt Davies <matt@mattdavies.dev>
2022-08-30 10:28:22 +0100

    Added boolean and string types

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log("Working");
+function add(num1, num2, showResult, showPhrase) {
+    if (showResult) {
+        console.log("".concat(showPhrase, " ").concat(num1 + num2));
+    }
+    else {
+        return num1 + num2;
+    }
+}
+var n1 = 55;
+var n2 = 5;
+var printResult = true;
+var phrase = "Result is:";
+add(n1, n2, printResult, phrase);
--- app.ts
+++ app.ts
@@ -1,8 +1,19 @@
-function add(num1: number, num2: number) {
+function add(
+  num1: number,
+  num2: number,
+  showResult: boolean,
+  showPhrase: string
+) {
+  if (showResult) {
+    console.log(`${showPhrase} ${num1 + num2}`);
+  } else {
     return num1 + num2;
+  }
 }
 
 const n1 = 55;
 const n2 = 5;
+const printResult = true;
+const phrase = "Result is:";
 
-const result = add(n1, n2);
+add(n1, n2, printResult, phrase);
